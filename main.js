8e1c5cc1e0a4c6def1bba3fb442ab5b8d1f5a4bd
luisbatista15-bit <luis.batista15@escola.pr.gov.br>
2026-07-09 09:26:33 -0300

    .

--- main.js
+++ main.js
@@ -67,7 +67,8 @@ function classificaSenha(tamanhoAlfabeto){
     } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
 const valorEntropia = document.querySelector('.entropia');
-valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+valorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24));
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
     }
 }
 
