814ada150e873b19242179f41bb99fbbdd18d8f1
luisbatista15-bit <luis.batista15@escola.pr.gov.br>
2026-07-09 09:24:46 -0300

    .

--- main.js
+++ main.js
@@ -66,6 +66,8 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('media');
     } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
     }
 }
 
