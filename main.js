6566fc977ebc19f59cf72c844b29750b1d338dd7
luisbatista15-bit <luis.batista15@escola.pr.gov.br>
2026-07-09 09:23:22 -0300

    .

--- main.js
+++ main.js
@@ -52,12 +52,13 @@ function geraSenha(){
         senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
-    classificaSenha();
+    classificaSenha(alfabeto.length);
 }
 
 
-function classificaSenha(){
-function classificaSenha(){
+function classificaSenha(tamanhoAlfabeto){
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
+    console.log(entropia);
     forcaSenha.classList.remove('fraca','media','forte');
     if (tamanhoSenha > 11){
         forcaSenha.classList.add('forte');
